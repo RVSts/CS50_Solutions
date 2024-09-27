d217662a18368c40bffe6f8b320d1287044e1917
RVSts <164586270+RVSts@users.noreply.github.com>
2024-09-27 01:53:25 +0000

    Thu, Sep 26, 2024, 10:53 PM -03:00

--- normals/6.sql
+++ normals/6.sql
@@ -1,5 +1,7 @@
-SELECT "50m", "latitude", "longitude" FROM "normals"
-WHERE ("latitude" LIKE '20%' AND "longitude" LIKE '55%') OR
-("latitude" LIKE '20%' AND "longitude" LIKE '75%') OR
-("latitude" LIKE '0%' AND "longitude" LIKE '55%') OR
-("latitude" LIKE '0%' AND "longitude" LIKE '75%');
+SELECT "50m", "latitude", "longitude"
+FROM "normals"
+WHERE
+    ("latitude" LIKE '20%' AND "longitude" LIKE '55%') OR
+    ("latitude" LIKE '20%' AND "longitude" LIKE '75%') OR
+    ("latitude" LIKE '0%' AND "longitude" LIKE '55%') OR
+    ("latitude" LIKE '0%' AND "longitude" LIKE '75%');
