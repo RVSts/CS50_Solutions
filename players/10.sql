74da9d2e9aaf18c51e4bbceafceb98c5b3bf6925
RVSts <164586270+RVSts@users.noreply.github.com>
2024-09-27 03:01:46 +0000

    Fri, Sep 27, 2024, 12:01 AM -03:00

--- players/10.sql
+++ players/10.sql
@@ -1,4 +1,4 @@
-SELECT "first_name", "last_name", AVG("height"), "height"
+SELECT "first_name", "last_name", "height"
 FROM "players"
 WHERE "height" > (SELECT AVG("height") FROM "players")
 ORDER BY "height" DESC, "first_name" ASC, "last_name" ASC;
