a3f6fc6d4365f55cec0807423201504fac9fe141
RVSts <164586270+RVSts@users.noreply.github.com>
2024-09-27 01:48:34 +0000

    Thu, Sep 26, 2024, 10:48 PM -03:00

--- normals/6.sql
+++ normals/6.sql
@@ -1,1 +1,5 @@
 SELECT "50m", "latitude", "longitude" FROM "normals"
+WHERE "latitude" LIKE '20%' AND "longitude" LIKE '55%' OR
+"latitude" = '20%' LIKE "longitude" LIKE '75%' OR
+"latitude" = '0%' LIKE "longitude" LIKE '55%' OR
+"latitude" = '0%' LIKE "longitude" LIKE '75%';
