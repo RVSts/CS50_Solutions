f269f55252699d9d1e98f4db4941c4bee50c0099
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-10 18:50:17 +0000

    Thu, Oct 10, 2024, 3:50 PM -03:00

--- packages/log.sql
+++ packages/log.sql
@@ -2,7 +2,7 @@
 -- *** The Lost Letter ***
 SELECT "action" FROM "scans"
 WHERE "package_id" IN(
-    SELECT "package_id" FROM "scans" WHERE(
+    SELECT "package_id" FROM "scans" WHERE "id" IN(
         SELECT "id" FROM "addresses" WHERE "from_address_id" = '900 Somerville Avenue' AND "to_address_id" = '2 Finnegan Street'
     )
 );
