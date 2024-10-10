f2b0583e9e949c6eda69dece14f0f0dc4a7778ff
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-10 18:36:44 +0000

    Thu, Oct 10, 2024, 3:36 PM -03:00

--- packages/log.sql
+++ packages/log.sql
@@ -1,7 +1,11 @@
 
 -- *** The Lost Letter ***
 SELECT "action" FROM "scans" WHERE(    -- First we need to stabilish what is being requested. Anekke needs to know if the letter was dropped off
-    SELECT "id" FROM "adresses" WHERE "from_address_id" = '900 Somerville Avenue' AND "to_address_id" = '2 Finnegan Street'
+    SELECT "ID" FROM "scans" WHERE(
+        SELECT "package_id" FROM "scans" WHERE(
+            SELECT "id" FROM "adresses" WHERE "from_address_id" = '900 Somerville Avenue' AND "to_address_id" = '2 Finnegan Street';
+        )
+    )
 )
 
 
