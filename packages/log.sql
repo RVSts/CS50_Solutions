3c6ea356267202e3404f63fb64d55bf07e0506a4
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-10 18:37:33 +0000

    Thu, Oct 10, 2024, 3:37 PM -03:00

--- packages/log.sql
+++ packages/log.sql
@@ -3,10 +3,10 @@
 SELECT "action" FROM "scans" WHERE(    -- First we need to stabilish what is being requested. Anekke needs to know if the letter was dropped off
     SELECT "ID" FROM "scans" WHERE(
         SELECT "package_id" FROM "scans" WHERE(
-            SELECT "id" FROM "adresses" WHERE "from_address_id" = '900 Somerville Avenue' AND "to_address_id" = '2 Finnegan Street';
+            SELECT "id" FROM "adresses" WHERE "from_address_id" = '900 Somerville Avenue' AND "to_address_id" = '2 Finnegan Street'
         )
     )
-)
+);
 
 
 
