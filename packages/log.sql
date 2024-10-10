9aecbdb1d482b248221976929ce81ef6f987ba4f
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-10 18:44:01 +0000

    Thu, Oct 10, 2024, 3:44 PM -03:00

--- packages/log.sql
+++ packages/log.sql
@@ -3,7 +3,7 @@
 SELECT "action" FROM "scans" WHERE(
     SELECT "ID" FROM "scans" WHERE(
         SELECT "package_id" FROM "scans" WHERE(
-            SELECT "id" FROM "adresses" WHERE "from_address_id" = '900 Somerville Avenue' AND "to_address_id" = '2 Finnegan Street'
+            SELECT "id" FROM "addresses" WHERE "from_address_id" = '900 Somerville Avenue' AND "to_address_id" = '2 Finnegan Street'
         )
     )
 );
