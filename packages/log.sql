dd7046b37182919e50d4df9c4c25c7cb654bb741
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-10 18:20:32 +0000

    Thu, Oct 10, 2024, 3:20 PM -03:00

--- packages/log.sql
+++ packages/log.sql
@@ -1,6 +1,8 @@
 
 -- *** The Lost Letter ***
-
+SELECT "action" FROM "scans" WHERE(    -- First we need to stabilish what is being requested. Anekke needs to know if the letter was dropped off
+    SELECT "address" FROM "adresses" WHERE "address" = 
+)
 
 
 
