d8becdd1500dc3a4283895d570380c487c53df69
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-11 01:39:03 +0000

    Thu, Oct 10, 2024, 10:39 PM -03:00

--- packages/log.sql
+++ packages/log.sql
@@ -1,5 +1,15 @@
 
 -- *** The Lost Letter ***
+
+-- The first step is to identify what is being requested: check the request action
+
+-- How can we know the request action: knowing the address "2 Finnegan Street"
+
+-- How can we get to the request action through the address: getting the address ID
+
+-- What can we get with the address ID: the ID of where the package is being sent, where, using the departure ID, we can get the package ID
+
+-- With this we get the ID of the scanned package, which gives us the scan ID and its respective action.
 SELECT "action" FROM "scans"
 WHERE "package_id" IN(
     SELECT "id" FROM "packages"
