093730e554fc426b63bd3b81c5804bb2f3c06348
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-11 01:30:30 +0000

    Thu, Oct 10, 2024, 10:30 PM -03:00

--- packages/log.sql
+++ packages/log.sql
@@ -1,18 +1,15 @@
 
 -- *** The Lost Letter ***
 SELECT "action" FROM "scans"
-WHERE(
-    SELECT "package_id" FROM "scans"
-    WHERE "package_id" IN(
-        SELECT "id" FROM "packages"
-        WHERE "to_address_id" IN(
-            SELECT "id" FROM "addresses"
-            WHERE "address" LIKE '%2 Fin%'
-        )
-        AND "from_address_id" IN(
-            SELECT "id" FROM "addresses"
-            WHERE "address" = '900 Somerville Avenue'
-        )
+WHERE "package_id" IN(
+    SELECT "id" FROM "packages"
+    WHERE "to_address_id" IN(
+        SELECT "id" FROM "addresses"
+        WHERE "address" LIKE '%2 Fin%'
+    )
+    AND "from_address_id" IN(
+        SELECT "id" FROM "addresses"
+        WHERE "address" = '900 Somerville Avenue'
     )
 );
 -- *** The Devious Delivery ***
