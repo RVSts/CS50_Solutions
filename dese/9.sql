be2c3c49cd941809cd9f4bd437fd007d6ce91d47
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-13 23:36:04 +0000

    Sun, Oct 13, 2024, 8:36 PM -03:00

--- dese/9.sql
+++ dese/9.sql
@@ -1,8 +1,7 @@
 SELECT "name" FROM "districts"
 WHERE "id" IN(
     SELECT "district_id" FROM "expenditures"
-    GROUP BY "pupils"
-    INTERSECT "district_id" WHERE "pupils" = "pupils"
-    ORDER BY "pupils" DESC
-    LIMIT 1
+    WHERE "pupils" IN(
+        SELECT MIN("pupils") FROM "expenditures"
+    )
 );
