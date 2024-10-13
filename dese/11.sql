33ff250c8e1f2e8e6c1bdd949c666eee25e9e081
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-13 23:50:21 +0000

    Sun, Oct 13, 2024, 8:50 PM -03:00

--- dese/11.sql
+++ dese/11.sql
@@ -1,1 +1,5 @@
 SELECT "name", "per_pupil_expenditure", "graduate"
+FROM "schools"
+JOIN "expenditures" ON "expenditures"."district_id" = "schools"."district_id"
+JOIN "graduation_rates" ON "graduation_rates"."school_id" = "schools"."id"
+ORDER BY "per_pupil_expenditure" DESC, "name" ASC;
