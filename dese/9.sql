1b42679c4accd013f8c2e1841b2e60e6e7b1982f
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-13 23:33:32 +0000

    Sun, Oct 13, 2024, 8:33 PM -03:00

--- dese/9.sql
+++ dese/9.sql
@@ -2,6 +2,7 @@ SELECT "name" FROM "districts"
 WHERE "id" IN(
     SELECT "district_id" FROM "expenditures"
     GROUP BY "pupils"
+    INTERSECT "district_id" WHERE "pupils" = "pupils"
     ORDER BY "pupils" DESC
-    LIMIT 10
+    LIMIT 1
 );
