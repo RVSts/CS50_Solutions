3604aea37c0d063be1ebf2d4a8aceb375e59c244
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-13 23:37:00 +0000

    Sun, Oct 13, 2024, 8:37 PM -03:00

--- dese/9.sql
+++ dese/9.sql
@@ -1,7 +1,7 @@
 SELECT "name" FROM "districts"
 WHERE "id" IN(
     SELECT "district_id" FROM "expenditures"
-    WHERE "pupils" IN(
+    WHERE "pupils" =(
         SELECT MIN("pupils") FROM "expenditures"
     )
 );
