e9f513a0e88213d4cfa9d336839915dee46cabbc
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-13 22:44:40 +0000

    Sun, Oct 13, 2024, 7:44 PM -03:00

--- dese/5.sql
+++ dese/5.sql
@@ -1,4 +1,6 @@
 SELECT COUNT("id"), "city" FROM "schools"
 WHERE "type" = 'Public School'
 GROUP BY "city"
-HAVING COUNT("id") 
+HAVING COUNT("id") <= 3
+ORDER BY COUNT("id") DESC, "city" ASC
+LIMIT 3;
