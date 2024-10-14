ce4c9afe4764faf531b0b76bc47b066dc45e9c7e
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-14 00:45:14 +0000

    Sun, Oct 13, 2024, 9:45 PM -03:00

--- dese/13.sql
+++ dese/13.sql
@@ -2,14 +2,14 @@ SELECT "staff_evaluations"."exemplary", "staff_evaluations"."unsatisfactory", "d
 FROM "districts"
 JOIN "staff_evaluations" ON "staff_evalutations"."district_id" = "districts"."id"
 JOIN "expenditures" ON "expenditures"."district_id" = "districts"."id"
-WHERE "type"."districts" = 'Public School District'
+WHERE "districts"."type" = 'Public School District'
 AND "staff_evaluations"."exemplary" >(
     SELECT AVG("exemplary") FROM "staff_evaluations"
 )
 AND "staff_evaluations"."unsatisfactory" >(
-    AVG("unsatisfactory") FROM "staff_evaluations"
+    SELECT AVG("unsatisfactory") FROM "staff_evaluations"
 )
 AND "expenditures"."per_pupil_expenditure">(
-    AVG("per_pupil_expenditure") FROM "expenditures"
+    SELECT AVG("per_pupil_expenditure") FROM "expenditures"
 )
 ORDER BY "unsatisfactory" DESC, "per_pupil_expenditure" DESC;
