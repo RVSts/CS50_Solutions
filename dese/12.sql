61bd2802bb8697e1f6737135b810c60484caae5f
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-14 00:03:29 +0000

    Sun, Oct 13, 2024, 9:03 PM -03:00

--- dese/12.sql
+++ dese/12.sql
@@ -3,10 +3,13 @@ JOIN "expenditures" ON "expenditures"."district_id" = "districts"."id"
 JOIN "staff_evaluations" ON "staff_evaluations"."district_id" = "districts"."id"
 WHERE "id" IN(
     SELECT "district_id" FROM "expenditures"
-    WHERE "per_pupil_expenditure" > AVG("per_pupil_expenditure")
+    GROUP BY "per_pupil_expenditure" >(
+        SELECT AVG("per_pupil_expenditure") FROM "expenditures"
+    )
 )
 AND "id" IN(
     SELECT "district_id" FROM "staff_evaluations"
-    WHERE "exemplary" > AVG("exemplary")
+    GROUP BY "exemplary" >(
+        SELECT AVG ("exemplary") FROM "exemplary"
 )
 ORDER BY "exemplary" DESC, "per_pupil_expenditure" DESC;
