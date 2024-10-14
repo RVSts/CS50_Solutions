d6ed3e3be69b52436bac9ef9b22fd0478f6d2118
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-14 00:38:30 +0000

    Sun, Oct 13, 2024, 9:38 PM -03:00

--- dese/13.sql
+++ dese/13.sql
@@ -1,1 +1,5 @@
-SELECT "
+SELECT "staff_evaluations"."exemplar", "staff_evaluations"."unsatisfactory", "districts"."name", "expenditures"."per_pupil_expenditure"
+FROM "districts"
+JOIN "staff_evaluations" ON "staff_evalutations"."district_id" = "districts"."id"
+JOIN "expenditures" ON "expenditures"."district_id" = "districts"."id"
+WHERE "type"."districts" = 
