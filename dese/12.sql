068776d7bcecd4e39f2f813bff00430bd150c1b9
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-14 00:24:26 +0000

    Sun, Oct 13, 2024, 9:24 PM -03:00

--- dese/12.sql
+++ dese/12.sql
@@ -1,7 +1,7 @@
 SELECT "expenditures"."per_pupil_expenditure", "staff_evaluations"."exemplary", "districts"."name" FROM "districts"
 JOIN "expenditures" ON "expenditures"."district_id" = "districts"."id"
 JOIN "staff_evaluations" ON "staff_evaluations"."district_id" = "districts"."id"
-WHERE "districts"."id" IN(
+WHERE "districts"."type" = 'Public School District' AND "districts"."id" IN(
     SELECT "district_id" FROM "expenditures"
     WHERE "per_pupil_expenditure" >(
         SELECT AVG("per_pupil_expenditure") FROM "expenditures"
