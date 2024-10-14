53c0dabb370e4bf8cedd28c093c5a5a9840e3a52
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-14 00:43:52 +0000

    Sun, Oct 13, 2024, 9:43 PM -03:00

--- dese/13.sql
+++ dese/13.sql
@@ -1,5 +1,15 @@
-SELECT "staff_evaluations"."exemplar", "staff_evaluations"."unsatisfactory", "districts"."name", "expenditures"."per_pupil_expenditure"
+SELECT "staff_evaluations"."exemplary", "staff_evaluations"."unsatisfactory", "districts"."name", "expenditures"."per_pupil_expenditure"
 FROM "districts"
 JOIN "staff_evaluations" ON "staff_evalutations"."district_id" = "districts"."id"
 JOIN "expenditures" ON "expenditures"."district_id" = "districts"."id"
-WHERE "type"."districts" = 'Charter Districts"
+WHERE "type"."districts" = 'Public School District'
+AND "staff_evaluations"."exemplary" >(
+    SELECT AVG("exemplary") FROM "staff_evaluations"
+)
+AND "staff_evaluations"."unsatisfactory" >(
+    AVG("unsatisfactory") FROM "staff_evaluations"
+)
+AND "expenditures"."per_pupil_expenditure">(
+    AVG("per_pupil_expenditure") FROM "expenditures"
+)
+ORDER BY "unsatisfactory" DESC, "per_pupil_expenditure" DESC;
