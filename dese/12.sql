acf930f83f24916fafc4119e3bdddd0453ec9bf6
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-14 00:21:11 +0000

    Sun, Oct 13, 2024, 9:21 PM -03:00

--- dese/12.sql
+++ dese/12.sql
@@ -1,16 +1,35 @@
-SELECT "expenditures"."per_pupil_expenditure", "staff_evaluations"."exemplary", "districts"."name" FROM "districts"
-JOIN "expenditures" ON "expenditures"."district_id" = "districts"."id"
-JOIN "staff_evaluations" ON "staff_evaluations"."district_id" = "districts"."id"
-WHERE "districts"."id" IN(
-    SELECT "district_id" FROM "expenditures"
-    WHERE "per_pupil_expenditure" >(
-        SELECT AVG("per_pupil_expenditure") FROM "expenditures"
+SELECT
+    "districts"."name",
+    "expenditures"."per_pupil_expenditure",
+    (SELECT
+         COUNT(*) FILTER (WHERE "rating" = 'exemplary') * 100.0 / COUNT(*)
+     FROM
+         "staff_evaluations"
+     WHERE
+         "district_id" = "districts"."id") AS "percentage_exemplary"
+FROM
+    "districts"
+JOIN
+    "expenditures" ON "expenditures"."district_id" = "districts"."id"
+WHERE
+    "expenditures"."per_pupil_expenditure" > (
+        SELECT AVG("per_pupil_expenditure")
+        FROM "expenditures"
     )
-)
-AND "districts"."id" IN(
-    SELECT "district_id" FROM "staff_evaluations"
-    WHERE "exemplary">(
-        SELECT AVG ("exemplary") FROM "staff_evaluations"
+AND
+    (SELECT
+         COUNT(*) FILTER (WHERE "rating" = 'exemplary') * 100.0 / COUNT(*)
+     FROM
+         "staff_evaluations"
+     WHERE
+         "district_id" = "districts"."id") > (
+        SELECT AVG(
+            (SELECT COUNT(*) FILTER (WHERE "rating" = 'exemplary') * 100.0 / COUNT(*)
+             FROM "staff_evaluations"
+             WHERE "district_id" = "staff_evaluations"."district_id")
+        )
+        FROM "staff_evaluations"
     )
-)
-ORDER BY "staff_evaluations"."exemplary" DESC, "expenditures"."per_pupil_expenditure" DESC;
+ORDER BY
+    "percentage_exemplary" DESC,
+    "expenditures"."per_pupil_expenditure" DESC;
