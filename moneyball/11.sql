a81c524f8e9a50b77d46cdafe2b52c58f6f1ae92
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-16 12:38:28 +0000

    Wed, Oct 16, 2024, 9:38 AM -03:00

--- moneyball/11.sql
+++ moneyball/11.sql
@@ -1,4 +1,4 @@
-SELECT "players"."first_name", "players"."last_name", ("salaries"."salary"/"performance"."H") AS "dollars per hit"
+SELECT "players"."first_name", "players"."last_name", ("salaries"."salary" / "performances"."H") AS "dollars per hit"
 FROM "players"
 JOIN "salaries" ON "salaries"."player_id" = "players"."id"
 JOIN "performances" ON "performances"."player_id" = "players"."id" AND "performances"."year" = "salaries"."year"
