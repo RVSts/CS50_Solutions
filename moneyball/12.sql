6f692ee20a97f580f5caa057c23156bcfdccbcb0
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-16 12:56:03 +0000

    Wed, Oct 16, 2024, 9:56 AM -03:00

--- moneyball/12.sql
+++ moneyball/12.sql
@@ -3,7 +3,7 @@ WHERE "id" IN(
     SELECT "players_id" FROM(
         SELECT ("salaries"."salary" / "performances"."RBI") AS "dollars per RBI" FROM "performances"
         JOIN "salaries" ON "salaries"."player_id" = "performances"."player_id" AND "salaries"."year" = "performances"."year"
-        WHERE "performances"."year" = '2001'
+        WHERE "performances"."year" = '2001' AND "performances"."RBI" > 0
         ORDER BY "dollars per RBI" ASC
         LIMIT 10
     )
