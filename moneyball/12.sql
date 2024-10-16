cd1a81f13737c8ef7ac80d8460c9779763817f92
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-16 13:08:09 +0000

    Wed, Oct 16, 2024, 10:08 AM -03:00

--- moneyball/12.sql
+++ moneyball/12.sql
@@ -1,16 +1,11 @@
 SELECT "first_name", "last_name" FROM "players"
 WHERE "id" IN(
-    SELECT "performances"."players_id" FROM(
-        SELECT "performances"."player_id", ("salaries"."salary" / "performances"."RBI") AS "dollars per RBI" FROM(
-            SELECT ("salaries"."salary" / "performances"."RBI") AS "dollars per RBI" FROM "performances"
-            JOIN "salaries" ON "salaries"."player_id" = "performances"."player_id" AND "salaries"."year" = "performances"."year"
-            WHERE "performances"."year" = '2001' AND "performances"."RBI" > 0
-            ORDER BY "dollars per RBI" DESC
-            LIMIT 10
-        ) AS subquery
+    SELECT "players_id" FROM(
+        SELECT "performances"."player_id" AS "players_id", ("salaries"."salary" / "performances"."RBI") AS "dollars per RBI" FROM "performances"
         JOIN "salaries" ON "salaries"."player_id" = "performances"."player_id" AND "salaries"."year" = "performances"."year"
         WHERE "performances"."year" = '2001' AND "performances"."RBI" > 0
         ORDER BY "dollars per RBI" ASC
+        LIMIT 10
     ) AS subquery
 )
 ORDER BY "id" ASC;
