b3c2b47d02f9cefb40cad85503d6890d24557c28
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-17 09:52:27 +0000

    Thu, Oct 17, 2024, 6:52 AM -03:00

--- moneyball/12.sql
+++ moneyball/12.sql
@@ -1,18 +1,18 @@
 SELECT "first_name", "last_name" FROM "players"
 WHERE "id" IN(
     SELECT "players_id" FROM(
-        SELECT "performances"."player_id" AS "players_id", ("salaries"."salary" / "performances"."RBI") AS "dollars per RBI" FROM "performances"
+        SELECT "performances"."player_id" AS "players_id", ("salaries"."salary" / "performances"."H") AS "dollars per hit" FROM "performances"
         JOIN "salaries" ON "salaries"."player_id" = "performances"."player_id" AND "salaries"."year" = "performances"."year"
-        WHERE "performances"."year" = '2001' AND "performances"."RBI" > 0
-        ORDER BY "dollars per RBI" ASC
+        WHERE "performances"."year" = '2001' AND "performances"."H" > 0
+        ORDER BY "dollars per hit" ASC
         LIMIT 10
     ) AS subquery
     INTERSECT
     SELECT "players_id" FROM(
-        SELECT "performances"."player_id" AS "players_id", ("salaries"."salary" / "performances"."H") AS "dollars per hit" FROM "performances"
+        SELECT "performances"."player_id" AS "players_id", ("salaries"."salary" / "performances"."RBI") AS "dollars per rbi" FROM "performances"
         JOIN "salaries" ON "salaries"."player_id" = "performances"."player_id" AND "salaries"."year" = "performances"."year"
         WHERE "performances"."year" = '2001' AND "performances"."RBI" > 0
-        ORDER BY "dollars per RBI" ASC
+        ORDER BY "dollars per rbi" ASC
         LIMIT 10
     ) AS subquery
 )
