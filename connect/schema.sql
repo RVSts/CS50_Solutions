50f4a0cc28c9b6f82d52be110bd36699256a18df
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-19 18:55:47 +0000

    Sat, Oct 19, 2024, 3:55 PM -03:00

--- connect/schema.sql
+++ connect/schema.sql
@@ -34,8 +34,8 @@ CREATE TABLE "connections_people"(
 
 CREATE TABLE "connections_schools"(
     "id" NUMERIC PRIMARY KEY,
-    "first_person_id" NUMERIC,
-    "second_person_id" NUMERIC,
-    FOREIGN KEY("first_person_id") REFERENCES "users"("id"),
-    FOREIGN KEY("second_person_id") REFERENCES "users"("id")
+    "school_id" NUMERIC,
+    "person_id" NUMERIC,
+    FOREIGN KEY("school_id") REFERENCES "users"("id"),
+    FOREIGN KEY("person_id") REFERENCES "users"("id")
 );
