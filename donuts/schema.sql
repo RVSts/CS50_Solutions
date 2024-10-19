45cd5e010c9b32d35f46436ff6a873325bdb095a
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-19 19:28:04 +0000

    Sat, Oct 19, 2024, 4:28 PM -03:00

--- donuts/schema.sql
+++ donuts/schema.sql
@@ -1,9 +1,19 @@
+CREATE TABLE "ingredients"(
+    "typical" TEXT,
+    "sugar" TEXT,
+    "typical_price" NUMERIC,
+    "sugar_price" NUMERIC
+);
+
 CREATE TABLE "donuts"(
     "name" TEXT,
     "type" TEXT,
-    "price" NUMERIC
-);
-
+    "price" NUMERIC,
+    "typical_used" TEXT,
+    "sugar_used" TEXT,
+    FOREING KEY ("typical_used") REFERENCES "ingredients"("typical"),
+    FOREING KEY ("sugar_used" REFERENCES "ingredients"("sugar")
+)
 CREATE TABLE "orders"(
     "numer" NUMERIC,
     "donuts_name" TEXT,
@@ -14,5 +24,5 @@ CREATE TABLE "customers"(
     "id" NUMERIC PRIMARY KEY,
     "first_name" TEXT,
     "last_name" TEXT,
-    
+
 )
