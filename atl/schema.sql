cd7b12c786748a53ff61a2713c56517cfc57ec75
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-19 18:06:00 +0000

    Sat, Oct 19, 2024, 3:06 PM -03:00

--- atl/schema.sql
+++ atl/schema.sql
@@ -1,11 +1,27 @@
 CREATE TABLE "passengers"(
+    "id" NUMERIC,
     "first_name" TEXT,
     "last_name" TEXT,
     "age" INTEGER
+    PRIMARY KEY("id")
 );
 
-CREAT TABLE "check-ins"(
+CREATE TABLE "check_ins"(
     "date" TEXT,
     "time" TEXT,
+    FOREIGN KEY ("passenger_id") REFERENCES "passengers"("id"),
+    FOREIGN KEY ("flight_id") REFERENCES "flights"("id")
+);
+
+CREATE TABLE "airlines"(
+    "name" TEXT,
+    "concourse" TEXT
+)
+
+CREATE TABLE "flights"(
+    "number" INTEGER,
+    "id" NUMERIC,
+    FOREIGN KEY ("airline_name") REFERENCES "airlines"("name"),
+    "code" TEXT,
     
 )
