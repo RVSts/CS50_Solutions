5d703c94c55ef8c0eb3ad978995859bebe5cd1b5
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-19 19:02:11 +0000

    Sat, Oct 19, 2024, 4:02 PM -03:00

--- connect/schema.sql
+++ connect/schema.sql
@@ -4,9 +4,7 @@ CREATE TABLE "users"(
     "last_name" TEXT,
     "username" TEXT,
     "password" TEXT,
-    "connection_people_id" NUMERIC,
-    FOREIGN KEY ("connection_people_id") REFERENCES "connections_people"("id"),
-
+    "connection_people_id" NUMERIC
 );
 
 CREATE TABLE "schools_universities"(
@@ -36,6 +34,21 @@ CREATE TABLE "connections_schools"(
     "id" NUMERIC PRIMARY KEY,
     "school_id" NUMERIC,
     "person_id" NUMERIC,
-    FOREIGN KEY("school_id") REFERENCES "users"("id"),
+    "start_date" TEXT,
+    "end_date" TEXT,
+    "type" TEXT,
+    FOREIGN KEY("school_id") REFERENCES "schools_universities"("id"),
     FOREIGN KEY("person_id") REFERENCES "users"("id")
 );
+
+CREATE TABLE "connections_companies"(
+    "id" NUMERIC PRIMARY KEY,
+    "companie_id" NUMERIC,
+    "person_id" NUMERIC,
+    "start_date" TEXT,
+    "end_date" TEXT,
+    "title" TEXT,
+    FOREIGN KEY("companie_id") REFERENCES "companies"("id"),
+    FOREIGN KEY("person_id") REFERENCES "users"("id")
+);
+
