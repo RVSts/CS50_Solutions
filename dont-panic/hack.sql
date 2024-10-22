f14ea34c555eeffc889090eb01ae3e889459d792
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-22 18:35:11 +0000

    Tue, Oct 22, 2024, 3:35 PM -03:00

--- dont-panic/hack.sql
+++ dont-panic/hack.sql
@@ -2,10 +2,10 @@ UPDATE "users" SET "password" = '982c0381c279d139fd221fce974916e7'
 WHERE "username" = 'admin';
 
 INSERT INTO "user_logs"("new_username", "new_password", "old_password", "type")
-VALUES('admin', '982c0381c279d139fd221fce974916e7', NULL, 'update');
+VALUES('admin', '982c0381c279d139fd221fce974916e7', NULL, 'insert');
 
 DELETE FROM "user_logs"
-WHERE "new_username" = 'admin' AND "type" = 'insert' AND "new_password" IS NOT NULL;
+WHERE "new_username" = 'admin' AND "type" = 'insert' AND "new_password" != '982c0381c279d139fd221fce974916e7';
 
 DELETE FROM "user_logs"
 WHERE "old_username" = 'admin' AND "type" = "update";
