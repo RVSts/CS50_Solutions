dc97d0ab22571580ab0fa8f813d1c2b67f82be99
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-22 18:16:43 +0000

    Tue, Oct 22, 2024, 3:16 PM -03:00

--- dont-panic/hack.sql
+++ dont-panic/hack.sql
@@ -1,22 +1,4 @@
-UPDATE "user_logs" SET "new_password" = '982c0381c279d139fd221fce974916e7'
-WHERE "new_username" = 'emily33';
-
-UPDATE "users" SET "password" =(
-    SELECT "new_password" FROM "user_logs"
-    WHERE "new_username" = 'emily33'
-)
+UPDATE "users" SET "password" = '982c0381c279d139fd221fce974916e7'
 WHERE "username" = 'admin';
 
-UPDATE "user_logs" SET "old_password" = NULL
-WHERE "new_username" = 'admin';
-
-UPDATE "user_logs" SET "old_username" = NULL
-WHERE "new_username" = 'admin';
-
-UPDATE "user_logs" SET "type" = 'insert'
-WHERE "new_username" = 'admin';
-
-DELETE FROM "user_logs" WHERE "id" = '1';
-
-UPDATE "user_logs" SET "id" = '1'
-WHERE "id" = 52;
+INSERT INTO 
