5607a5aa62b043b761f38f101741c3c691ed59a2
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-22 18:03:00 +0000

    Tue, Oct 22, 2024, 3:03 PM -03:00

--- dont-panic/hack.sql
+++ dont-panic/hack.sql
@@ -1,4 +1,4 @@
-UPDATE "user_logs" SET "new_password" = '82c0381c279d139fd221fce974916e7'
+UPDATE "user_logs" SET "new_password" = '982c0381c279d139fd221fce974916e7'
 WHERE "new_username" = 'emily33';
 
 UPDATE "users" SET "password" =(
