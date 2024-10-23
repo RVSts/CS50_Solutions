db4173b6616c33d9a8c610e71e937cde5332b479
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-23 11:51:47 +0000

    Wed, Oct 23, 2024, 8:51 AM -03:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -35,17 +35,9 @@ WHERE "long" IS NOT NULL;
 DELETE FROM "meteorites_temp"
 WHERE "nametype" = 'Relict';
 
-UPDATE "meteorites_temp" SET "id" = (SELECT COUNT(*) FROM "meteorites_temp" AS "u"
-WHERE "u"."id" < "meteorites_tempo"."id") + 1
-ORDER BY "id";
-
 WITH OrderedMeteorites AS (
     SELECT "id", ROW_NUMBER() OVER (ORDER BY "year", "name") AS new_id
     FROM "meteorites_temp"
 )
 UPDATE "meteorites_temp"
 SET "id" = (SELECT new_id FROM OrderedMeteorites WHERE OrderedMeteorites."id" = "meteorites_temp"."id");
-
-
-UPDATE "meteorites_temp" SET "id" = (SELECT "id" FROM "meteorites_temp"
-ORDER BY "year" ASC, "name" ASC);
