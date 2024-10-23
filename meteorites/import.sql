7cdf93370346f91008d720a647e95bbe398a8337
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-23 12:30:10 +0000

    Wed, Oct 23, 2024, 9:30 AM -03:00

--- meteorites/import.sql
+++ meteorites/import.sql
@@ -1,4 +1,4 @@
-CREATE TABLE meteorites(
+CREATE TABLE "meteorites_temp"(
     "name" TEXT,
     "id" INTEGER PRIMARY KEY,
     "nametype" TEXT,
@@ -10,43 +10,32 @@ CREATE TABLE meteorites(
     "long" REAL
 );
 
-CREATE TABLE meteorites_temp(
-    "name" TEXT,
-    "id" INTEGER PRIMARY KEY,
-    "nametype" TEXT,
-    "class" TEXT,
-    "mass" REAL,
-    "discovery" TEXT,
-    "year" INTEGER,
-    "lat" REAL,
-    "long" REAL
-);
 
-.mode csv
 .import --csv --skip 1 meteorites.csv meteorites_temp
 
+
 UPDATE "meteorites_temp" SET "mass" = NULL
-WHERE "mass" = "0";
+WHERE "mass" = 0;
 
 UPDATE "meteorites_temp" SET "lat" = NULL
-WHERE "lat" = "0";
+WHERE "lat" = 0;
 
 UPDATE "meteorites_temp" SET "long" = NULL
-WHERE "long" = "0";
+WHERE "long" = 0;
 
-INSERT INTO "meteorites" ("name", "class", "mass", "discovery", "year", "lat", "long")
-SELECT
-    "name",
-    "class",
-    ROUND("mass", 2) AS "mass",
-    "discovery",
-    "year",
-    ROUND("lat", 2) AS "lat",
-    ROUND("long", 2) AS "long"
-FROM
-    "meteorites_temp"
-WHERE "nametype" != 'Relict'
-ORDER BY "year" ASC, "name" ASC;
+UPDATE "meteorites_temp" SET "mass" = ROUND("mass", 2), "long" = ROUND("long", 2), "long" = ROUND("long", 2);
 
--- Renomear a tabela
-DROP TABLE "meteorites_temp";
+CREATE TABLE "meteorites"(
+    "name" TEXT,
+    "id" INTEGER PRIMARY KEY,
+    "nametype" TEXT,
+    "class" TEXT,
+    "mass" REAL,
+    "discovery" TEXT,
+    "year" INTEGER,
+    "lat" REAL,
+    "long" REAL
+);
+
+INSERTO INTO "meteorites"("name", "class", "mass", "discovery, "year", "lat", "long")
+
