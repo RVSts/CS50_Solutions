4f4200da4d39023152a880354b817d225d11d9f7
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-26 16:21:29 +0000

    Sat, Oct 26, 2024, 1:21 PM -03:00

--- private/private.sql
+++ private/private.sql
@@ -2,5 +2,5 @@ CREATE VIEW "message" AS
 SELECT "sentence" AS "phrase" FROM "sentences"
 WHERE "id" IN(
     SELECT "id" FROM "sentences"
-    WHERE "id" = '14' OR "id" = '114' OR 'id' 
+    WHERE "id" = '14' OR "id" = '114' OR 'id'
 )
