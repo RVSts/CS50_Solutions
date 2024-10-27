718969b45815ced88633838cfab9c50135ce8bd8
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-27 03:58:00 +0000

    Sun, Oct 27, 2024, 12:58 AM -03:00

--- bnb/june_vacancies.sql
+++ bnb/june_vacancies.sql
@@ -1,5 +1,5 @@
 CREATE VIEW "june_vacancies" AS
-SELECT "listings"."id", "listings"."property_type", "listings"."host_name", COUNT("availabilities"."available") AS "days_vacant"
+SELECT "listings"."id", "listings"."property_type", "listings"."host_name", COUNT("availabilities"."date") AS "days_vacant"
 FROM "listings"
 JOIN "availabilities" ON "availabilities"."id" = "listings"."id"
 WHERE "available" = 'TRUE' AND "availabilities"."date" LIKE '2023-06%'
