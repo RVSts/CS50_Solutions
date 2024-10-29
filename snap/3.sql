a47e716e2eafb00a81ddaa7a83a9442d7d077a88
RVSts <164586270+RVSts@users.noreply.github.com>
2024-10-29 17:05:38 +0000

    Tue, Oct 29, 2024, 2:05 PM -03:00

--- snap/3.sql
+++ snap/3.sql
@@ -1,3 +1,6 @@
 EXPLAIN QUERY PLAN
-SELECT "id" FROM "users"
-WHERE "
+SELECT "to_user_id" FROM "messages"
+WHERE "from_user_id" =(
+    SELECT "id" FROM "users"
+    WHERE "username" = 'creativewisdom377'
+)
