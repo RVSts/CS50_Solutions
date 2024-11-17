3494873b249cc681fa08a28586e08e350d1b4003
Rian Vasconcelos <164586270+RVSts@users.noreply.github.com>
2024-11-17 11:07:14 +0000

    Sun, Nov 17, 2024, 8:07 AM -03:00

---
+++ harvard/indexes.sql
@@ -0,0 +1,7 @@
+SELECT "courses"."title", "courses"."semester"
+FROM "enrollments"
+JOIN "courses" ON "enrollments"."course_id" = "courses"."id"
+JOIN "students" ON "enrollments"."student_id" = "students"."id"
+WHERE "students"."id" = 3;
+
+CREATE INDEX "
