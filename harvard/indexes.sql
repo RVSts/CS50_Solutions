a45e7e1af18b7460167e10aef457fc97cbcc7fec
Rian Vasconcelos <164586270+RVSts@users.noreply.github.com>
2024-11-17 11:22:49 +0000

    Sun, Nov 17, 2024, 8:22 AM -03:00

--- harvard/indexes.sql
+++ harvard/indexes.sql
@@ -1,10 +1,5 @@
-EXPLAIN QUERY PLAN
-SELECT "courses"."title", "courses"."semester"
-FROM "enrollments"
-JOIN "courses" ON "enrollments"."course_id" = "courses"."id"
-JOIN "students" ON "enrollments"."student_id" = "students"."id"
-WHERE "students"."id" = 3;
-
 CREATE INDEX "student_index" ON "students"("id");
 
 DROP INDEX "student_index";
+
+CREATE INDEX "
