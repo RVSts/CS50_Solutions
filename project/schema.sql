edca4ef1f1d2ee37da7d2a293fca994940896e02
Rian Vasconcelos <164586270+RVSts@users.noreply.github.com>
2024-12-02 17:38:42 +0000

    Mon, Dec 2, 2024, 2:38 PM -03:00

--- project/schema.sql
+++ project/schema.sql
@@ -2,5 +2,15 @@
 
 CREATE TABLE Supermarkets(
     id PRIMARY KEY smallserial
+    supermarket_name varchar(30)
+    address varchar(120)
+);
+
+CREATE TABLE Customers(
+    id PRIMARY KEY serial
+    first_name varchar(30)
+    last_name varchar(60)
+    username varchar(30)
+    email varchar(100)
     
 )
