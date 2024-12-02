c64954a0a9cf1fd2fcd135022d64304ac7764ab0
Rian Vasconcelos <164586270+RVSts@users.noreply.github.com>
2024-12-02 17:33:58 +0000

    Mon, Dec 2, 2024, 2:33 PM -03:00

--- project/schema.sql
+++ project/schema.sql
@@ -1,3 +1,5 @@
 -- In this SQL file, write (and comment!) the schema of your database, including the CREATE TABLE, CREATE INDEX, CREATE VIEW, etc. statements that compose it
 
-CREATE TABLE
+CREATE TABLE Supermarkets(
+    id PRIMARY KEY 
+)
