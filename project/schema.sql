257d85ea7fa8ed9a6f536f1500b8a9fd0370c8d0
Rian Vasconcelos <164586270+RVSts@users.noreply.github.com>
2024-12-03 19:18:04 +0000

    Tue, Dec 3, 2024, 4:18 PM -03:00

--- project/schema.sql
+++ project/schema.sql
@@ -41,7 +41,7 @@ CREATE TABLE Trademarks (
 CREATE TABLE Categories (
     id PRIMARY KEY smallserial,
     category_name varchar(20)
-)
+);
 
 CREATE TABLE Products (
     id PRIMARY KEY serial,
@@ -57,41 +57,77 @@ CREATE TABLE Products (
     category_id smallserial NOT NULL,
     supermarket_id smallserial NOT NULL,
     FOREIGN KEY (supplier_id) REFERENCES TO Suppliers(id),
-    FOREIGN KEY (category_id) REFERENCES TO 
-    FOREIGN KEY (supermarket_id) REFERENCES TO Supermarkets(id),
-
-)
+    FOREIGN KEY (category_id) REFERENCES TO,
+    FOREIGN KEY (supermarket_id) REFERENCES TO Supermarkets(id)
+);
 
 
 CREATE TABLE Price_History (
-
-)
+    id PRIMARY KEY serial,
+    price numeric(4,2),
+    date_time datetime,
+    product_id serial NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES TO Products(id)
+);
 
 CREATE TABLE Departments (
-
-)
+    id PRIMARY KEY smallserial,
+    department_name VARCHAR(20),
+    supermarket_id smallserial NOT NULL,
+    FOREIGN KEY (supermarket_id) REFERENCES TO Supermarkets(id)
+);
 
 CREATE TABLE Job_Titles (
-
-)
+    id PRIMARY KEY smallserial,
+    job_title_name varchar(20),
+    department_id smallserial NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES TO Departments(id)
+);
 
 CREATE TABLE Employees (
-
-)
+    id PRIMARY KEY smallserial,
+    first_name varchar(30),
+    last_name varchar(60),
+    job_title_id smallserial NOT NULL,
+    starting_date date,
+    ending_date date,
+    supermarket_id smallserial NOT NULL,
+    department_id smallserial NOT NULL,
+    FOREIGN KEY (supermarket_id) REFERENCES TO Supermarkets(id),
+    FOREIGN KEY (department_id) REFERENCES TO Departments(id)
+);
 
 CREATE TABLE Promotions (
-
-)
+    id PRIMARY KEY smallserial,
+    type_name,
+    price numeric(4,2),
+    starting_date datetime,
+    ending_date datetime,
+    supermarket_id smallserial NOT NULL,
+    trademark_id smallserial NOT NULL,
+    products_id serial NOT NULL,
+    FOREIGN KEY (supermarket_id) REFERENCES TO Supermarkets(id),
+    FOREIGN KEY (trademark_id) REFERENCES TO Trademarks(id),
+    FOREIGN KEY (product_id) REFERENCES TO Products(id)
+);
 
 
 CREATE TABLE Sales (
-
-)
+    id PRIMARY KEY bigserial,
+    date_time datetime,
+    customer_id serial NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES TO Customers(id)
+);
 
 CREATE TABLE Sales_Products (
-
-)
+    id PRIMARY KEY bigserial,
+    date_time datetime,
+    sales_id bigserial NOT NULL,
+    product_id serial NOT NULL,
+    quantity smallint,
+    unit_price numeric(4,2)
+);
 
 CREATE TABLE Reviews (
 
-)
+);
