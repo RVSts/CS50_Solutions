b281185cb576be72829a8fc82b0961f9cd44a20f
Rian Vasconcelos <164586270+RVSts@users.noreply.github.com>
2024-12-04 02:01:08 +0000

    Tue, Dec 3, 2024, 11:01 PM -03:00

--- project/schema.sql
+++ project/schema.sql
@@ -109,42 +109,42 @@ CREATE TABLE Promotions (
     price numeric(4,2),
     starting_date timestamp,
     ending_date timestamp,
-    supermarket_id smallserial NOT NULL,
-    trademark_id smallserial NOT NULL,
-    products_id serial NOT NULL,
-    FOREIGN KEY (supermarket_id) REFERENCES TO Supermarkets(id),
-    FOREIGN KEY (trademark_id) REFERENCES TO Trademarks(id),
-    FOREIGN KEY (product_id) REFERENCES TO Products(id)
+    supermarket_id smallint NOT NULL CHECK (supermarket_id >= 0),
+    trademark_id smallint NOT NULL CHECK (trademark_id >= 0),
+    products_id int NOT NULL CHECK (products_id >= 0),
+    FOREIGN KEY (supermarket_id) REFERENCES Supermarkets(id),
+    FOREIGN KEY (trademark_id) REFERENCES Trademarks(id),
+    FOREIGN KEY (product_id) REFERENCES Products(id)
 );
 
 
 CREATE TABLE Sales (
-    id PRIMARY KEY bigserial,
-    date_time datetime,
-    customer_id serial NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES TO Customers(id)
+    id bigserial PRIMARY KEY,
+    date_time timestamp,
+    customer_id int NOT NULL CHECK (customer_id >= 0),
+    FOREIGN KEY (customer_id) REFERENCES Customers(id)
 );
 
 CREATE TABLE Sales_Products (
-    id PRIMARY KEY bigserial,
-    date_time datetime,
-    sales_id bigserial NOT NULL,
-    product_id serial NOT NULL,
+    id bigserial PRIMARY KEY,
+    date_time timestamp,
+    sales_id bigint NOT NULL CHECK (sales_id >= 0),
+    product_id int NOT NULL CHECK (product_id >= 0),
     quantity smallint,
     unit_price numeric(4,2),
-    FOREIGN KEY (sales_id) REFERENCES TO Sales(id),
-    FOREIGN KEY (product_id) REFERENCES TO Products(id)
+    FOREIGN KEY (sales_id) REFERENCES Sales(id),
+    FOREIGN KEY (product_id) REFERENCES Products(id)
 );
 
 CREATE TABLE Reviews (
-    id PRIMARY KEY serial,
-    review_date datetime,
-    rate int check (rate between 1 and 5),
+    id serial PRIMARY KEY,
+    review_date timestamp,
+    rate int CHECK (rate between 1 and 5),
     comment varchar(500),
-    customer_id serial NOT NULL,
-    supermarket_id smallserial NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES TO Customers(id),
-    FOREIGN KEY (supermarket_id) REFERENCES TO Supermarkets(id)
+    customer_id int NOT NULL CHECK (customer_id >= 0),
+    supermarket_id smallint NOT NULL (supermarket_id >= 0),
+    FOREIGN KEY (customer_id) REFERENCES Customers(id),
+    FOREIGN KEY (supermarket_id) REFERENCES Supermarkets(id)
 );
 
 
