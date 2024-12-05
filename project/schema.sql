5fa02adb094a6c7a605d21ae6f9e5af06ded6920
Rian Vasconcelos <164586270+RVSts@users.noreply.github.com>
2024-12-05 21:58:23 +0000

    Thu, Dec 5, 2024, 6:58 PM -03:00

--- project/schema.sql
+++ project/schema.sql
@@ -2,14 +2,12 @@
 CREATE TYPE unit_of_measure_type AS ENUM ('kg', 'g', 'l', 'ml', 'unit');
 CREATE TYPE promotion_type AS ENUM ('discount', 'bundle');
 
-
-
 -- Tables without dependencies
 CREATE TABLE Supermarkets (
     id smallserial,
     name varchar(30),
     address varchar(120),
-    CONSTRAINT suppermarket_key PRIMARY KEY (id)
+    CONSTRAINT supermarket_key PRIMARY KEY (id)
 );
 
 CREATE TABLE Suppliers (
@@ -28,8 +26,8 @@ CREATE TABLE Trademarks (
     id smallserial,
     name varchar(30),
     supplier_id smallint NOT NULL,
-    CONSTRAINT trademark_key PRIMARY KEY (id),
-    FOREIGN KEY (supplier_id) REFERENCES Suppliers(id) -- Reference to Suppliers
+    FOREIGN KEY (supplier_id) REFERENCES Suppliers (id),
+    CONSTRAINT trademark_key PRIMARY KEY (id)
 );
 
 -- Customers table
@@ -48,22 +46,23 @@ CREATE TABLE Supermarkets_Customers (
     id serial,
     supermarket_id smallint NOT NULL,
     customer_id int NOT NULL,
-    CONSTRAINT sup_cust_key PRIMARY KEY (id),
-    FOREIGN KEY (supermarket_id) REFERENCES Supermarkets(id),
-    FOREIGN KEY (customer_id) REFERENCES Customers(id)
+    FOREIGN KEY (supermarket_id) REFERENCES Supermarkets (id),
+    FOREIGN KEY (customer_id) REFERENCES Customers (id),
+    CONSTRAINT sup_cust_key PRIMARY KEY (id)
 );
 
 CREATE TABLE Supermarket_Supplier (
-    id smallserial PRIMARY KEY,
+    id smallserial,
     supermarket_id smallint NOT NULL,
     supplier_id smallint NOT NULL,
-    FOREIGN KEY (supermarket_id) REFERENCES Supermarkets(id),
-    FOREIGN KEY (supplier_id) REFERENCES Suppliers(id)
+    FOREIGN KEY (supermarket_id) REFERENCES Supermarkets (id),
+    FOREIGN KEY (supplier_id) REFERENCES Suppliers (id),
+    CONSTRAINT supermarket_supplier_key PRIMARY KEY (id)
 );
 
 -- Products table
 CREATE TABLE Products (
-    id serial PRIMARY KEY,
+    id serial,
     name varchar(60),
     price numeric(6,2) NOT NULL CHECK (price >= 0),
     quantity smallint NOT NULL CHECK (quantity >= 0),
@@ -75,41 +74,45 @@ CREATE TABLE Products (
     trademark_id smallint NOT NULL,
     category_id smallint NOT NULL,
     supermarket_id smallint NOT NULL,
-    FOREIGN KEY (trademark_id) REFERENCES Trademarks(id),
-    FOREIGN KEY (category_id) REFERENCES Categories(id),
-    FOREIGN KEY (supermarket_id) REFERENCES Supermarkets(id)
+    FOREIGN KEY (trademark_id) REFERENCES Trademarks (id),
+    FOREIGN KEY (category_id) REFERENCES Categories (id),
+    FOREIGN KEY (supermarket_id) REFERENCES Supermarkets (id),
+    CONSTRAINT product_key PRIMARY KEY (id)
 );
 
 -- Price History table
 CREATE TABLE Price_History (
-    id serial PRIMARY KEY,
+    id serial,
     price numeric(4,2),
     date_time timestamp,
     product_id int NOT NULL,
     is_active boolean DEFAULT TRUE,
-    FOREIGN KEY (product_id) REFERENCES Products(id)
+    FOREIGN KEY (product_id) REFERENCES Products (id),
+    CONSTRAINT price_history_key PRIMARY KEY (id)
 );
 
 -- Departments table
 CREATE TABLE Departments (
-    id smallserial PRIMARY KEY,
+    id smallserial,
     name varchar(20),
     supermarket_id smallint NOT NULL,
-    FOREIGN KEY (supermarket_id) REFERENCES Supermarkets(id),
-    UNIQUE(name, supermarket_id) -- Ensure unique department names per supermarket
+    FOREIGN KEY (supermarket_id) REFERENCES Supermarkets (id),
+    UNIQUE (name, supermarket_id),
+    CONSTRAINT department_key PRIMARY KEY (id)
 );
 
 -- Job Titles table
 CREATE TABLE Job_Titles (
-    id smallserial PRIMARY KEY,
+    id smallserial,
     name varchar(20),
     department_id smallint NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES Departments(id)
+    FOREIGN KEY (department_id) REFERENCES Departments (id),
+    CONSTRAINT job_title_key PRIMARY KEY (id)
 );
 
 -- Employees table
 CREATE TABLE Employees (
-    id smallserial PRIMARY KEY,
+    id smallserial,
     first_name varchar(30),
     last_name varchar(60),
     job_title_id smallint NOT NULL,
@@ -117,14 +120,15 @@ CREATE TABLE Employees (
     ending_date date,
     supermarket_id smallint NOT NULL,
     department_id smallint NOT NULL,
-    FOREIGN KEY (supermarket_id) REFERENCES Supermarkets(id),
-    FOREIGN KEY (department_id) REFERENCES Departments(id)
+    FOREIGN KEY (supermarket_id) REFERENCES Supermarkets (id),
+    FOREIGN KEY (department_id) REFERENCES Departments (id),
+    CONSTRAINT employee_key PRIMARY KEY (id)
 );
 
 -- Promotions table
 CREATE TABLE Promotions (
-    id smallserial PRIMARY KEY,
-    type promotion_type NOT NULL, -- Define the correct type for name
+    id smallserial,
+    type promotion_type NOT NULL,
     price numeric(4,2) NOT NULL CHECK (price >= 0),
     starting_date timestamp NOT NULL,
     ending_date timestamp NOT NULL,
@@ -132,45 +136,48 @@ CREATE TABLE Promotions (
     trademark_id smallint NOT NULL,
     product_id int NOT NULL,
     is_active boolean DEFAULT TRUE,
-    FOREIGN KEY (supermarket_id) REFERENCES Supermarkets(id),
-    FOREIGN KEY (trademark_id) REFERENCES Trademarks(id),
-    FOREIGN KEY (product_id) REFERENCES Products(id),
-    CHECK (starting_date < ending_date)
+    FOREIGN KEY (supermarket_id) REFERENCES Supermarkets (id),
+    FOREIGN KEY (trademark_id) REFERENCES Trademarks (id),
+    FOREIGN KEY (product_id) REFERENCES Products (id),
+    CHECK (starting_date < ending_date),
+    CONSTRAINT promotion_key PRIMARY KEY (id)
 );
 
 -- Sales table
 CREATE TABLE Sales (
-    id bigserial PRIMARY KEY,
+    id bigserial,
     date_time timestamp,
     customer_id int NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customers(id)
+    FOREIGN KEY (customer_id) REFERENCES Customers (id),
+    CONSTRAINT sales_key PRIMARY KEY (id)
 );
 
 -- Sales Products table
 CREATE TABLE Sales_Products (
-    id bigserial PRIMARY KEY,
+    id bigserial,
     date_time timestamp,
     sales_id bigint NOT NULL,
     product_id int NOT NULL,
     quantity smallint NOT NULL CHECK (quantity >= 0),
     unit_price numeric(4,2) NOT NULL CHECK (unit_price >= 0),
-    FOREIGN KEY (sales_id) REFERENCES Sales(id),
-    FOREIGN KEY (product_id) REFERENCES Products(id)
+    FOREIGN KEY (sales_id) REFERENCES Sales (id),
+    FOREIGN KEY (product_id) REFERENCES Products (id),
+    CONSTRAINT sales_products_key PRIMARY KEY (id)
 );
 
 -- Reviews table
 CREATE TABLE Reviews (
-    id serial PRIMARY KEY,
+    id serial,
     date timestamp,
-    rate int CHECK (rate between 1 and 5),
+    rate int CHECK (rate BETWEEN 1 AND 5),
     comment varchar(500),
     customer_id int NOT NULL,
     supermarket_id smallint NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customers(id),
-    FOREIGN KEY (supermarket_id) REFERENCES Supermarkets(id),
-    UNIQUE(customer_id, supermarket_id) -- Ensure one review per customer per supermarket
+    FOREIGN KEY (customer_id) REFERENCES Customers (id),
+    FOREIGN KEY (supermarket_id) REFERENCES Supermarkets (id),
+    UNIQUE (customer_id, supermarket_id),
+    CONSTRAINT review_key PRIMARY KEY (id)
 );
-
 
 -- Indexes for Supermarkets_Customers table
 CREATE INDEX idx_supermarkets_customers_supermarket_id ON Supermarkets_Customers (supermarket_id);
@@ -183,8 +190,3 @@ CREATE INDEX idx_price_history_product_id ON Price_History (product_id);
 CREATE INDEX idx_sales_products_sales_id ON Sales_Products (sales_id);
 CREATE INDEX idx_sales_products_product_id ON Sales_Products (product_id);
 CREATE INDEX idx_products_expiration_date ON Products (expiration_date);
-
-
--- Roles for user restrictions
-
-CREATE ROLE
